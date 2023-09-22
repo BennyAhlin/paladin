8b143b6fc95cef6b80d78e5c0ac50eaba1455d7d
BennyAhlin <Benny.Ahlin@hotmail.com>
2023-09-22 11:21:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello, please enter");
+    string paladin = Console.ReadLine();
+string reverse = string.Empty;
+Console.Clear();
+for (int i = (paladin.Length - 1); i >= 0; i--) 
+{
+
+    reverse += paladin[i].ToString();
+    //Måste ha + och = för att lägga ihop index igen!
+}
+
+if ( reverse == paladin)
+{
+   Console.WriteLine("this is a paladin");
+}
+else
+{
+    Console.WriteLine("this is a warlock");
+}
+
+Console.WriteLine(); 
+Console.WriteLine(paladin);
+Console.WriteLine(reverse);
